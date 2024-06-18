34acec4952c05105e3181e41f12ff5e75c4ab063
mahmoud sheta <shetamahmoud463@gmail.com>
2024-06-18 14:15:26 -0700

    Second biggest!

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -2,8 +2,7 @@
 const { argv } = require('node:process');
 if (argv.length <= 3) {
   console.log(0);
+} else {
+  const list = argv.sort();
+  console.log(list.reverse()[1]);
 }
-const args = argv.slice(2);
-const numbers = args.map(arg => Number(arg));
-const sortsNumbers = numbers.sort((a, b) => b - a);
-console.log(sortsNumbers[1]);
