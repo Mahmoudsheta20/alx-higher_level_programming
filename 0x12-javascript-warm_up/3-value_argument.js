885d94c1286d5905941b862b02a7f0cc36097589
mahmoud sheta <shetamahmoud463@gmail.com>
2024-06-18 06:18:43 -0700

    Value of my argument

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -4,7 +4,9 @@ const { argv } = require('node:process');
 // print process.argv
 if (argv[2]) {
   argv.forEach((val, index) => {
-    console.log(val);
+    if (index > 1) {
+	  console.log(val);
+    }
   });
 } else {
   console.log('No argument');
