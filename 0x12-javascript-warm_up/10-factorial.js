4298fb464707de4705e80957b113929840a0e95c
mahmoud sheta <shetamahmoud463@gmail.com>
2024-06-18 12:53:39 -0700

    Factorial

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
 const { argv } = require('node:process');
 
-const factorial = (n) => {
+function factorial (n) {
   if (isNaN(n) || n <= 1) {
     return 1;
   }
   return n * factorial(n - 1);
-};
+}
 
 console.log(factorial(Number(argv[2])));
