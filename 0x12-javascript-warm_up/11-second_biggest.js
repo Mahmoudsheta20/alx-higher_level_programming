5856a193261ad227a41cc266739c75a0735134bd
mahmoud sheta <shetamahmoud463@gmail.com>
2024-06-18 15:06:11 -0700

    Add file

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,8 +1,18 @@
-#!/usr/bin/node
+#!/usr/bin/env node
 
-if (process.argv.length <= 3) {
+// Get command line arguments, excluding the first two (node executable and script path)
+const args = process.argv.slice(2);
+
+// Check the number of arguments
+if (args.length <= 1) {
   console.log(0);
 } else {
-  const list = process.argv.sort();
-  console.log(list.reverse()[1]);
+  // Convert arguments to integers
+  const numbers = args.map(arg => parseInt(arg, 10));
+
+  // Remove duplicates and sort the numbers in descending order
+  const uniqueNumbers = [...new Set(numbers)].sort((a, b) => b - a);
+
+  // Print the second biggest number
+  console.log(uniqueNumbers[1]);
 }
