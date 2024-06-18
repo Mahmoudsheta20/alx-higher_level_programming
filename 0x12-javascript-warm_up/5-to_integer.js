481c144e0bd7db172e3f51b8f1781d3f8166072f
mahmoud sheta <shetamahmoud463@gmail.com>
2024-06-18 08:49:16 -0700

    An Integer

---
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+const { argv } = require('node:process');
+if (Number(argv[2])) {
+  console.log(`My number: ${Math.floor(Number(argv[2]))}`);
+} else {
+  console.log('Not a number');
+}
