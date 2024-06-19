6e3197aba7d1be110c122b5daacee7e528985063
mahmoud sheta <shetamahmoud463@gmail.com>
2024-06-19 06:31:03 -0700

    Rectangle #3

---
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+module.exports = class Rectangle {
+  constructor (w, h) {
+    if (w && h && Math.sign(w) === 1 && Math.sign(h) === 1) {
+      this.width = w;
+	    this.height = h;
+    } else {
+      this.width = undefined;
+      this.height = undefined;
+    }
+  }
+};
