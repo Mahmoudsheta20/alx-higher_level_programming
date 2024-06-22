41c679a9330117640189f68400ea3ac3504bcdbb
Mahmoud Sheta <shetamahmoud463@gmail.com>
2024-06-22 22:35:31 +0300

    SOLVE

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,6 +1,6 @@
 -- 9-full_creation.sql
 CREATE TABLE second_table (id INT,name VARCHAR(256), score INT)
-INSERT INTO  second_table (id ,name, score) VALUES
+INSERT INTO  `second_table` (`id` ,`name`, `score`) VALUES
 (
  1,'John',10
 ),
